ba8bf2589901726ce4fcfcdb7840ffa64e788cfc
Danielgbit <danielbitmobb@gmail.com>
2023-08-11 11:33:15 -0500

    Add models, data: JsonProducts & Fix gitignore/Node_modules

--- EcoRootWeb/src/app.js
+++ EcoRootWeb/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const dotenv = require('dotenv').config();
+const methodOverride = require('method-override');
 
 //ROUTES
 
@@ -9,25 +10,6 @@ const mainRoute = require ('./routes/mainRoute');
 const productsRoute = require ('./routes/productsRoute');
 const cartRoute = require ('./routes/cartRoute');
 const userRoute = require ('./routes/userRoute');
-const createRoute = require ('./routes/createRoute');
-
-
-app.listen(3000, () => {
-    console.log('servidor ' + process.env.PORT + ' en funcionamiento - http://localhost:3000');
-});
-
-
-app.use(express.static('./public'));
-
-app.use('/', mainRoute);
-
-app.use('/cart', cartRoute);
-
-app.use('/products', productsRoute);
-
-app.use('/user', userRoute );
-
-app.use('/create', createRoute);
 
 
 app.set('view engine', 'ejs');
@@ -37,10 +19,30 @@ app.set('views', [
     path.join(__dirname, './views/partials'),
     path.join(__dirname, './views/products'),
     path.join(__dirname, './views/user'),
-    path.join(__dirname, './views/productsCreate'),
-
-  ]);
+]);
 
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('./public'));
+
+app.use(methodOverride('_method'));
+
+app.use('/', mainRoute);
+
+app.use('/cart', cartRoute);
+
+app.use('/products', productsRoute);
+
+app.use('/user', userRoute );
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('servidor ' + process.env.PORT + ' en funcionamiento - http://localhost:3000');
+});
 
 
--- EcoRootWeb/src/routes/productsRoute.js
+++ EcoRootWeb/src/routes/productsRoute.js
@@ -1,12 +1,39 @@
-const express = require ('express');
+const express = require('express');
 const productsRouter = express.Router();
-const productsController = require ('../controllers/productsController');
+const productsController = require('../controllers/productsController');
 
 
-
+// @GET /products
 productsRouter.get('/', productsController.products);
 
+productsRouter.post('/', productsController.postProductCreate);
+
+
+// @GET /products/id/detail
 productsRouter.get('/:id/detail', productsController.productDetail);
+
+// CREATE
+
+// @GET /products/create
+productsRouter.get('/create', productsController.getproductCreate);
+
+
+// @DELETE /products/:id/delete
+productsRouter.delete('/:id/delete', productsController.productDestroy);
+
+//EDIT
+
+
+// @GET /products/:id/edit
+productsRouter.get('/:id/edit', productsController.getEdit);
+
+// @GET /products/:id/detail
+productsRouter.put('/:id/detail', productsController.putProductEdit);
+
+
+
+
+
 
 
 
