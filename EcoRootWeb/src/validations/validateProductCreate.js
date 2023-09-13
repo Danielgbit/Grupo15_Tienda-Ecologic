baedac8490aa995d6375c9cbe7999f2249578b55
federicoavilaa <avilapelagattifederico@gmail.com>
2023-09-12 22:47:26 -0300

    validaciones no necesarias en productos

--- EcoRootWeb/src/validations/validateProductCreate.js
+++ EcoRootWeb/src/validations/validateProductCreate.js
@@ -9,9 +9,9 @@ const productCreateMiddleware = {
     productCreateValidations: [     
         body('name').notEmpty().withMessage('Debes ingresar un nombre'),
         body('brand').notEmpty().withMessage('Debes ingresar una marca'),
-        body('color').notEmpty().withMessage('Debes ingresar un color'),
+/*         body('color').notEmpty().withMessage('Debes ingresar un color'), */
         body('united').notEmpty().withMessage('Debes ingresar una unidad de stock'),
-        body('discount').notEmpty().withMessage('campo incompleto'),
+/*         body('discount').notEmpty().withMessage('campo incompleto'), */
         body('material').notEmpty().withMessage('Debes ingresar el material de tu producto'),
         body('state').notEmpty().withMessage('Debes ingresar el estado de tu producto'),
         body('description').notEmpty().withMessage('Debes ingresar una descripción'),
